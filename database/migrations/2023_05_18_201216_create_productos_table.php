8332ccd2dfb1677acdff5d8992f028214978eb8a
profeLozano <lozano.hernan@latecnicalf.com.ar>
2023-05-18 19:20:59 -0300

    muestra modelo y migracion

---
+++ database/migrations/2023_05_18_201216_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 60);
+            $table->string('categoria', 40);
+            $table->string('color', 20)->nullable();
+            $table->decimal('peso', 6,3)->nullable();
+            $table->string('n_serie')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
